d941ef025628ad8305d9b88dc3dfc728f54babe5
robertthome <remedy42290@gmail.com>
2022-09-04 02:01:30 -0700

    updated states in Miantimer

--- workouttimer/src/components/MainTimer.jsx
+++ workouttimer/src/components/MainTimer.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 
 function MainTimer() {
-  const [timer, setTimer] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
   const [timerName, setTimerName] = useState(0) //maybe its own component?
 
   return (
     <div>
       <h1>Hey there little bear, why the long face?</h1>
+      <h3>
+        <span>0</span>:00
+      </h3>
     </div>
   )
 }
