53dc01ac7f41c0ab63e758133cb502e2f18f3cea
robertthome <remedy42290@gmail.com>
2022-09-04 16:24:55 -0700

    logic for basic upcount timer in Maintimer component

--- workouttimer/src/components/MainTimer.jsx
+++ workouttimer/src/components/MainTimer.jsx
@@ -1,16 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function MainTimer() {
   const [minutes, setMinutes] = useState(0)
   const [seconds, setSeconds] = useState(0)
   const [timerName, setTimerName] = useState(0) //maybe its own component?
 
+  let timer
+
+  useEffect(() => {
+    timer = setInterval(() => {
+      setSeconds(seconds + 1)
+
+      if (seconds === 59) {
+        setMinutes(minutes + 1)
+        setSeconds(0)
+      }
+    }, 1000)
+
+    return () => clearInterval(timer)
+  })
+
   return (
     <div>
       <h1>Hey there little bear, why the long face?</h1>
-      <h3>
-        <span>0</span>:00
-      </h3>
+      <h2>
+        {minutes < 10 ? '0' + minutes : minutes}:
+        {seconds < 10 ? '0' + seconds : seconds}
+      </h2>
     </div>
   )
 }
